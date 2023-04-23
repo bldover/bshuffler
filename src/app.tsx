be569e7144b7758b6537cce7fa992a6c442e2a0e
Bradley Dover <dover.bradley@gmail.com>
2023-04-23 16:34:49 -0400

    show context menu action

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,34 @@
-async function main() {
-  while (!Spicetify?.showNotification) {
-    await new Promise(resolve => setTimeout(resolve, 100));
+async function initShuffler() {
+
+  let { Type } = Spicetify.URI;
+
+  let shuffleableTypes = [
+      Type.ALBUM,
+      Type.ARTIST,
+      Type.COLLECTION,
+      Type.FOLDER,
+      Type.PLAYLIST,
+      Type.PLAYLIST_V2
+  ];
+
+  function isShuffleable(uris: string[]) {
+    let uriType = Spicetify.URI.fromString(uris[0]).type;
+    return shuffleableTypes.includes(uriType);
   }
-  
-  // Show message on start.
-  Spicetify.showNotification("Hello!");
+
+  async function shuffleAndPlay(uri: string) {
+    Spicetify.showNotification("Adding to menu!");
+  }
+
+  new Spicetify.ContextMenu.Item(
+      "bShuffle",
+      async uris => {
+        await shuffleAndPlay(uris[0]);
+      },
+      isShuffleable,
+      "shuffle"
+  ).register();
+  console.log("Loaded bShuffler!");
 }
 
-export default main;
+export default initShuffler;
